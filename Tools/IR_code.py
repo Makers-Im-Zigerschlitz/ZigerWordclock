47607cf2300424020c244f7ca93e0d62867abe41
morgulbrut <tillo.bosshart@gmail.com>
2022-11-20 21:53:39 +0100

    Renamed files

--- Tools/IR_code.py
+++ Tools/IR_code.py
@@ -1,9 +1,12 @@
 import pulseio
 import adafruit_irremote
 import settings
+import colours
 import adafruit_dotstar
 import time
+import IR_codes
 
+IR_LEARN = True
 
 print('IR listener')
 # Fuzzy pulse comparison function:
@@ -27,25 +30,25 @@ pulses.resume()
 pixels = adafruit_dotstar.DotStar(settings.LED_PIN_CI, settings.LED_PIN_DI, settings.LED_NUMBER, brightness=settings.LED_DEFAULT_BRIGHTNESS, auto_write=True)
 
 
-if not settings.IR_LEARN:
+if not IR_LEARN:
     # Loop waiting to receive pulses.
     while True:
         # Wait for a pulse to be detected.
         detected = decoder.read_pulses(pulses,blocking_delay=0.1)
         print('got a pulse...')
         # Got a pulse, now compare.
-        if fuzzy_pulse_compare(settings.IR_1, detected):
+        if fuzzy_pulse_compare(IR_codes.CODE_1, detected):
             print('Recieved ON')
             pixels.brightness = 0.1
-        if fuzzy_pulse_compare(settings.IR_2, detected):
+        if fuzzy_pulse_compare(IR_codes.CODE_2, detected):
             print('Recieved OFF')
             pixels.brightness = 0            
-        if fuzzy_pulse_compare(settings.IR_3, detected):
+        if fuzzy_pulse_compare(IR_codes.CODE_3, detected):
             print('Recieved RED')
-            pixels.fill(settings.LED_COLOUR_RED)
-        if fuzzy_pulse_compare(settings.IR_4, detected):
+            pixels.fill(colours.RED)
+        if fuzzy_pulse_compare(IR_codes.CODE_4, detected):
             print('Recieved GREEN')
-            pixels.fill(settings.LED_COLOUR_GREEN)
+            pixels.fill(colours.GREEN)
         
 else:
     while True:
@@ -55,8 +58,10 @@ else:
             code = decoder.decode_bits(detected)
             print("Decoded:", code)
         except adafruit_irremote.IRNECRepeatException:  # unusual short code!
-            print("NEC repeat!")
+            print("NEC repeat")
         except adafruit_irremote.IRDecodeException as e:     # failed to decode
             print("Failed to decode: ", e.args)
+        except:
+            print("Unknown error, retry")
 
         print("----------------------------")
